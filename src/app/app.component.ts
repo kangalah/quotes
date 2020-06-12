6cc3105feafa97c7558394de52dfdea1f30bd2bd
kangalah <nyongesajoannanjala@gmail.com>
2020-06-12 23:05:05 +0300

    change title of the import class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes:string[];
-  authors:string[];
-
-
-  constructor(){
-    this.quotes = ['Never quit', 'Happiness comes from sharing']
-    this.authors = ['Joan Nanjala', 'Hannington Curl']
-  } 
+  title = 'quotes';
 }
