a6d312fa373cef8b87f8f16fe98ed733489db80b
kangalah <nyongesajoannanjala@gmail.com>
2020-06-13 15:51:57 +0300

    add new quotes with the authors

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  quotes=[new Quote("ANC","Julius Malema","Mama give us a sign"),
+  new Quote("Lupita","Black Panther","Wakanda Forever")]
+    preNum:number
+    lastNum:number
+    counter:number
+  
+    addQuote(emittedQuote){
+      this.quotes.push(emittedQuote)
+    }
+  
+    upvote(i){
+        this.quotes[i].upvotes+=1
+    }
+    downvote(i){
+      this.quotes[i].downvotes+=1
+    }
+    delQuote(i){
+      this.quotes.splice(i, 1)
+    }
+    highestUpvote(){
+      this.preNum = 0
+      this.lastNum = 0
+  
+      for(this.counter=0 ; this.counter < this.quotes.length; this.counter++) {
+        this.lastNum = this.quotes[this.counter].upvotes;
+        if(this.lastNum > this.preNum){this.preNum = this.lastNum}
+      }
+      return  this.preNum
+    }
+    constructor() { }
+  
+    ngOnInit() {
+    }
+  
   }
-
-}
