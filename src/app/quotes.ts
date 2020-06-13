829ef131aaf03a2da0b04dd46dce0486e5bc96cc
kangalah <nyongesajoannanjala@gmail.com>
2020-06-13 15:54:26 +0300

    add name, author and quote constructor

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,4 +1,10 @@
 export class Quotes {
-    id: number;
-    name: string;
+    public upvotes:number
+    public downvotes:number
+    public mydate:Date
+    constructor (public name:string, public author:string, public quote:string){
+        this.upvotes=0
+        this.downvotes=0
+        this.mydate= new Date()
+    }
 }
