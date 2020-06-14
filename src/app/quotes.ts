87b0c5448903ca13b16344afd848d345dd4a8281
kangalah <nyongesajoannanjala@gmail.com>
2020-06-15 00:43:31 +0300

    add dateCount pipe

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -2,7 +2,7 @@ export class Quotes {
     public upvotes:number
     public downvotes:number
     public mydate:Date
-    constructor (public name:string, public author:string, public quote:string){
+    constructor (public name:string, public quote:string, public author:string,){
         this.upvotes=0
         this.downvotes=0
         this.mydate= new Date()
